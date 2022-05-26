6d2b9100310ecaced7ba6aa6fc26887ddcc95b23
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-05-26 12:25:15 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as txt:
+    long_description = txt.read()
+
+setuptools.setup(
+    name='kissmanga',
+    version='1.0.2',
+    description='An Unofficial Python API Library to Download Manga  for FREE...',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='metavoid',
+    author_email='metavoidteam@gmail.com',
+    url='https://github.com/moezilla/kissmangaapi.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bs4", "requests", "requests_html"],
+    python_requires='>=3.6'
+)
